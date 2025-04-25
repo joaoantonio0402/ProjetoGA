3c5322317051067c7dd5ffcb2c1d4c4bb821c609
Gabriel Farias <gabrielpfarias0611@gmail.com>
2025-04-25 01:05:43 -0300

    agora vai

--- ProjetoGA/ProjetoGA/TelaInventario.h
+++ ProjetoGA/ProjetoGA/TelaInventario.h
@@ -13,14 +13,20 @@ public:
 	void adicionarItemInventario(string);
 	void removerItemInventario(string);
 	void substituirItem(string, string); // Substitui um item equipado por um item do inventario
-	void adicionarItemEquipado(Item);
-	void removerItemEquipado(Item);
+	void adicionarItemEquipado(string);
+	void removerItemEquipado(string);
 	void imprimirInventario();
 	void modificarInventario();
+	void adicionarProvisao();
+	void usarProvisao();
 private:
 	string tipoPersonagem; //Valor esperado como parametro
+	string arquivo; //Armazena o nome do arquivo do guerreiro ou mago
+	int valorTesouro; // Armazena o valor de tesouro
+	int cntProvisoes; // Armazena a quantidade de provisoes
 	int habilidade; //Valor esperado como parametro
 	int energia; //Valor esperado como parametro
+	int energiaMax; //Valor maximo de energia do personagem
 	int sorte; //Valor esperado como parametro
 	int cntItensInventario; // Contador itens no inventario
 	int cntItensEquipados; // Contador itens equipados
@@ -28,6 +34,7 @@ private:
 	vector<string> itensEquipados; // Array que armazena os itens equipados
 	vector<string> linhasArquivo; // Array que armazena as string que irao virar as linhas do arquivo
 	void atualizarItens(); // Funcao que atualiza os itens do inventario
+	void bufferParaArquivo(); // Move linhasArquivo para o arquivo
 	
 };
 
